6bedbc320eb9e9859d2d284c8b2e36cf1f2fdf9d
Suhela <suhelafarhat@gmail.com>
2018-02-02 20:46:29 +0530

    Changes

--- src/JavaRobot.java
+++ src/JavaRobot.java
@@ -7,20 +7,27 @@ public class JavaRobot
 
     public static void main(String[] args) 
     {
+        if(args.length==0)
+        {
+            System.out.println("Please give 4 inputs for X-input, Y-input, Direction and Command");
+            System.exit(0);
+        }
         int xDir = parseInt(args[0]);
         int yDir = parseInt(args[1]);
         int tem;
+        int flag=0;
         
         String sDir = args[2];
         String sCommand = args[3];
         String[] sClockwise = {"North","East","South","West"};
         String[] sAntiClockwise = {"North","West","South","East"};
+        String sSteps = "";
         
         char[] cCommands = sCommand.toCharArray();
         
         for(int i=0;i<cCommands.length;i++)
         {
-            if(cCommands[i]=='R')
+            if(cCommands[i]=='R' || cCommands[i]=='r')
             {
                 for(int j=0;j<sClockwise.length;j++)
                 {
@@ -41,8 +48,9 @@ public class JavaRobot
                         }
                     }
                 }
+                flag=1;
             }
-            if(cCommands[i]=='L')
+            if(cCommands[i]=='L' || cCommands[i]=='l')
             {
                 for(int j=0;j<sAntiClockwise.length;j++)
                 {
@@ -63,22 +71,47 @@ public class JavaRobot
                         }
                     }
                 }
+                flag=1;
             }
-            if(cCommands[i]=='W')
+            if(cCommands[i]=='W' || cCommands[i]=='w')
             {
-                i++;
-                tem = Character.getNumericValue(cCommands[i]);
-                switch(sDir)
+                try
                 {
-                    case "South" : yDir = yDir - tem;
-                                   break;
-                    case "North" : yDir = yDir + tem;
-                                   break;
-                    case "East"  : xDir = xDir + tem;
-                                   break;
-                    case "West"  : xDir = xDir - tem;
-                                   break; 
+                    i++;
+                    sSteps = sSteps+cCommands[i];
+                    while(i<cCommands.length-1 && (Character.isDigit(cCommands[i+1])))
+                    {
+                        sSteps = sSteps+cCommands[i+1];
+                        i++;
+                    }                                              
+                    tem = parseInt(sSteps);
+                    switch(sDir.toUpperCase())
+                    {
+                        case "SOUTH" : yDir = yDir - tem;
+                                       flag=1; 
+                                       break;
+                        case "NORTH" : yDir = yDir + tem;
+                                       flag=1; 
+                                       break;
+                        case "EAST"  : xDir = xDir + tem;
+                                       flag=1; 
+                                       break;
+                        case "WEST"  : xDir = xDir - tem;
+                                       flag=1; 
+                                       break; 
+                    }
                 }
+                catch(Exception e)
+                {
+                    System.out.println(e);
+                    System.out.println("Not a valid input. Please give proper arguments");
+                }
+                
+            }
+            if(flag==0)
+            {
+                System.out.println("Not a valid input. Please give proper arguments");
+                System.exit(0);
             }
         }
         System.out.print(xDir + " " + yDir + " " + sDir + "\n");
